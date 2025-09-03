4d34d79676c5d3445b53dabbaff15cbf4b68d654
James Wright <jamie.wright125@gmail.com>
2025-09-03 02:13:20 +0100

    Day 8 Initialise

---
+++ Day_08/Day_08.py
@@ -0,0 +1,25 @@
+from formatting_fns import *
+import numpy as np
+
+# import file
+
+##########################
+part1_start = start_part_1(1)
+######### PART 1 #########
+
+part1_answer = 0
+
+# part 1 code
+
+##########################
+end_part(part1_answer, part1_start, 1)
+part2_start = start_part_2()
+######### PART 2 #########
+
+part2_answer = 0
+
+# part 2 code
+
+##########################
+end_part(part2_answer, part2_start, 2)
+##########################
